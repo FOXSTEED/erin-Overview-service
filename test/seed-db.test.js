1652165900052f85deff730a6b68e5e2dc6908e8
Eric M Sandoval <ericmsandoval@gmail.com>
2018-02-26 11:32:23 -0800

    Tests for urls returned by async random-puppy

--- test/seed-db.test.js
+++ test/seed-db.test.js
@@ -5,9 +5,23 @@ describe('Seeding of database', () => {
   test('Should contain 200 items', () => {
     Attraction.count((err, count) => {
       if (err) {
-        console.log(err);
+        console.log('ERR retrieveing count', err);
       }
       expect(count).toEqual(200);
     });
   });
+
+  test('A doc should contain 3 photo urls', () => {
+    Attraction.findOne((err, doc) => {
+      if (err) {
+        console.log('ERR retrieving doc', err);
+      }
+      doc.photos.forEach((obj) => {
+        expect(obj).toHaveProperty('url');
+        expect(typeof obj.url).toBe('string');
+        expect(obj.url).toEqual(expect.stringContaining(imgur));
+      });
+    });
+  });
 });
+
