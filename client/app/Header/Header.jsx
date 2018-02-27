b721a46392001f3f779b8beb1d7a8d576e94f5a4
Eric M Sandoval <ericmsandoval@gmail.com>
2018-02-27 10:26:46 -0800

    Configures webpack to allow CSS modules in react components

--- client/app/Header/Header.jsx
+++ client/app/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './Header.css';
+
 const Header = props => (
   <div>
     <h1>{props.attr.name}</h1>
-    <div>{props.attr.description}</div>
+    <div className={styles.test}>{props.attr.description}</div>
   </div>);
 
 Header.propTypes = { attr: PropTypes.object.isRequired };
