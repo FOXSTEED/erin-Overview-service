abc23c61546864d75dd50f3c5ccef542d43f399f
Eric M Sandoval <ericmsandoval@gmail.com>
2018-02-26 21:12:19 -0800

    Fixes testing bug - tests are breakable and actually testing

--- test/seed-db.test.js
+++ test/seed-db.test.js
@@ -1,17 +1,23 @@
 /* eslint no-undef: "off" */
 const { Attraction } = require('../db/attraction.js');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/overview');
 
 describe('Seeding of database', () => {
-  test('Should contain 200 items', () => {
+  test('Should contain 200 items', (done) => {
+    expect.assertions(1);
     Attraction.count((err, count) => {
       if (err) {
-        console.log('ERR retrieveing count', err);
+        console.log(err);
       }
       expect(count).toEqual(200);
+      done();
     });
   });
 
-  test('A doc should contain 3 photo urls', () => {
+  test('A doc should contain 3 photo urls', (done) => {
+    expect.assertions(9);
     Attraction.findOne((err, doc) => {
       if (err) {
         console.log('ERR retrieving doc', err);
@@ -19,8 +25,9 @@ describe('Seeding of database', () => {
       doc.photos.forEach((obj) => {
         expect(obj).toHaveProperty('url');
         expect(typeof obj.url).toBe('string');
-        expect(obj.url).toEqual(expect.stringContaining(imgur));
+        expect(obj.url).toEqual(expect.stringContaining('imgur'));
       });
+      done();
     });
   });
 });
