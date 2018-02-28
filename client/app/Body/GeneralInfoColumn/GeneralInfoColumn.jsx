f62c84e422e9c3136bf6ce87d64220e6eb8b5cc4
Eric M Sandoval <ericmsandoval@gmail.com>
2018-02-28 10:27:57 -0800

    Filling in general info column in body component

--- client/app/Body/GeneralInfoColumn/GeneralInfoColumn.jsx
+++ client/app/Body/GeneralInfoColumn/GeneralInfoColumn.jsx
@@ -5,8 +5,25 @@ import styles from './GeneralInfoColumn.css';
 
 const GeneralInfoColumn = props => (
   <div className={styles.column}>
-    <div className={styles.section}>Hours</div>
-    <div className={styles.section}>Location</div>
+    <div className={styles.section}>
+      <div>
+        <span className={styles.clock}>**</span>
+        <span className={styles.open}>OPEN?</span>
+        <a>All Hours</a>
+      </div>
+      <div className={styles.hoursToday}>Hours Today: needs updating!</div>
+      <div className={styles.duration}>
+        <span className={styles.clock}>**</span>
+        <span>Suggested Duration: X hours</span>
+      </div>
+    </div>
+    <div className={[styles.section, styles.middle].join(' ')}>
+      <div className={styles.title}>LOCATION</div>
+      <div className={styles.addContainer}>
+        <div className={styles.clock}>**</div>
+        <div>Full address will be very long should go here make sure this text is long enough</div>
+      </div>
+    </div>
     <div className={styles.section}>Contact</div>
   </div>
 );
