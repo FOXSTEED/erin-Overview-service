6e0e8153fcf30fefaf98993fbbeccb4ed3b37e70
Eric M Sandoval <ericmsandoval@gmail.com>
2018-03-01 13:54:21 -0800

    Connects map component with Google Maps API; shows country from address

--- client/app/Body/Map/Map.jsx
+++ client/app/Body/Map/Map.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './Map.css';
+import map from './maps_api_key.js';
+import styles from './Map.css'
 
-const Map = props => (
-  <div>Map</div>
+const Map = ({ location }) => (
+  <iframe
+    className={styles.map}
+    title="map"
+    frameBorder="0"
+    src={`https://www.google.com/maps/embed/v1/place?key=${map.key}&q=${location}`}
+  />
 );
 
-Map.propTypes = {};
+Map.propTypes = {
+  location: PropTypes.string.isRequired,
+};
 
 export default Map;
