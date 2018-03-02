f0bbe25c311f198903f4f6e80b59b1013b5df63c
Eric M Sandoval <ericmsandoval@gmail.com>
2018-03-01 16:53:59 -0800

    Refactor of RatingsChart code to eliminate repetition

--- client/app/Body/RatingsColumn/RatingsChart/RatingsChart.jsx
+++ client/app/Body/RatingsColumn/RatingsChart/RatingsChart.jsx
@@ -3,34 +3,27 @@ import PropTypes from 'prop-types';
 
 import styles from './RatingsChart.css';
 
-const RatingsChart = ({ ratings, reviews }) => (
-  <ul className={styles.chart}>
-    <li className={styles.chartRow}>
-      <span className={[styles.cell, styles.word].join(' ')} >Excellent</span>
-      <span className={[styles.cell, styles.bar].join(' ')} ><span className={styles.fill} /></span>
-      <span className={[styles.cell, styles.num].join(' ')}>{Math.floor((ratings[0] / reviews) * 100)}%</span>
-    </li>
-    <li className={styles.chartRow}>
-      <span className={[styles.cell, styles.word].join(' ')}>Very Good</span>
-      <span className={[styles.cell, styles.bar].join(' ')} ><span className={styles.fill} /></span>
-      <span className={[styles.cell, styles.num].join(' ')}>{Math.floor((ratings[1] / reviews) * 100)}%</span>
-    </li>
-    <li className={styles.chartRow}>
-      <span className={[styles.cell, styles.word].join(' ')}>Average</span>
-      <span className={[styles.cell, styles.bar].join(' ')} ><span className={styles.fill} /></span>
-      <span className={[styles.cell, styles.num].join(' ')}>{Math.floor((ratings[2] / reviews) * 100)}%</span>
-    </li>
-    <li className={styles.chartRow}>
-      <span className={[styles.cell, styles.word].join(' ')}>Poor</span>
-      <span className={[styles.cell, styles.bar].join(' ')} ><span className={styles.fill} /></span>
-      <span className={[styles.cell, styles.num].join(' ')}>{Math.floor((ratings[3] / reviews) * 100)}%</span>
-    </li>
-    <li className={styles.chartRow}>
-      <span className={[styles.cell, styles.word].join(' ')}>Terrible</span>
-      <span className={[styles.cell, styles.bar].join(' ')} ><span className={styles.fill} /></span>
-      <span className={[styles.cell, styles.num].join(' ')}>{Math.floor((ratings[4] / reviews) * 100)}%</span>
-    </li>
-  </ul>
+const RatingsChart = ({ ratings, reviews }) => {
+  const ratingWords = ['Excellent', 'Very Good', 'Average', 'Poor', 'Terrible'];
+
+  return (
+    <ul className={styles.chart}>
+      {ratings.map((rating, index) => (
+        <ChartRow
+          percentage={Math.floor((rating / reviews) * 100)}
+          ratingWord={ratingWords[index]}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const ChartRow = ({ percentage, ratingWord }) => (
+  <li className={styles.chartRow}>
+    <span className={[styles.cell, styles.word].join(' ')} >{ratingWord}</span>
+    <span className={[styles.cell, styles.bar].join(' ')} ><span className={styles.fill} style={{ width: `${percentage}%` }} /></span>
+    <span className={[styles.cell, styles.num].join(' ')}>{percentage}%</span>
+  </li>
 );
 
 RatingsChart.propTypes = {
@@ -38,5 +31,9 @@ RatingsChart.propTypes = {
   reviews: PropTypes.number.isRequired,
 };
 
+ChartRow.propTypes = {
+  percentage: PropTypes.number.isRequired,
+  ratingWord: PropTypes.string.isRequired,
+};
+
 export default RatingsChart;
-  
