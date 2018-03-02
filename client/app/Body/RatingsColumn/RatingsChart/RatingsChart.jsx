c6620785c3d819f31885069211f0009648a877fd
Eric M Sandoval <ericmsandoval@gmail.com>
2018-03-01 17:09:31 -0800

    Highlights ratingWord with highest percentage

--- client/app/Body/RatingsColumn/RatingsChart/RatingsChart.jsx
+++ client/app/Body/RatingsColumn/RatingsChart/RatingsChart.jsx
@@ -5,6 +5,15 @@ import styles from './RatingsChart.css';
 
 const RatingsChart = ({ ratings, reviews }) => {
   const ratingWords = ['Excellent', 'Very Good', 'Average', 'Poor', 'Terrible'];
+  let max = 0;
+  let indexOfMax = null;
+
+  ratings.forEach((rating, i) => {
+    if (rating > max) {
+      max = rating;
+      indexOfMax = i;
+    }
+  });
 
   return (
     <ul className={styles.chart}>
@@ -12,16 +21,17 @@ const RatingsChart = ({ ratings, reviews }) => {
         <ChartRow
           percentage={Math.floor((rating / reviews) * 100)}
           ratingWord={ratingWords[index]}
+          max={index === indexOfMax}
         />
       ))}
     </ul>
   );
 };
 
-const ChartRow = ({ percentage, ratingWord }) => (
+const ChartRow = ({ percentage, ratingWord, max }) => (
   <li className={styles.chartRow}>
-    <span className={[styles.cell, styles.word].join(' ')} >{ratingWord}</span>
-    <span className={[styles.cell, styles.bar].join(' ')} ><span className={styles.fill} style={{ width: `${percentage}%` }} /></span>
+    <span className={[styles.cell, styles.word].join(' ')} style={max ? { color: '#00a680' } : {}}>{ratingWord}</span>
+    <span className={[styles.cell, styles.bar].join(' ')}><span className={styles.fill} style={{ width: `${percentage}%` }} /></span>
     <span className={[styles.cell, styles.num].join(' ')}>{percentage}%</span>
   </li>
 );
@@ -34,6 +44,7 @@ RatingsChart.propTypes = {
 ChartRow.propTypes = {
   percentage: PropTypes.number.isRequired,
   ratingWord: PropTypes.string.isRequired,
+  max: PropTypes.bool,
 };
 
 export default RatingsChart;
