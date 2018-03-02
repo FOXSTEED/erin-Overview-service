db9bd4a2be95da95d9a26baec5a34bcc1998a9d0
Eric M Sandoval <ericmsandoval@gmail.com>
2018-03-02 15:33:39 -0800

    Mvp of photos component

--- client/app/Body/RatingsColumn/TalkAbout/TalkAbout.jsx
+++ client/app/Body/RatingsColumn/TalkAbout/TalkAbout.jsx
@@ -8,19 +8,19 @@ const TalkAbout = ({ talkAbout }) => (
     <div className={styles.title}>TRAVELERS TALK ABOUT</div>
     <div className={styles.keywords}>
       <div className={styles.keywordRow}>
-        <img src={talkAbout[0].avatar} alt="userPhoto" />
+        <img className={styles.user} src={talkAbout[0].avatar} alt="userPhoto" />
         <div className={styles.text}>
           <a className={styles.phrase}>"{talkAbout[0].phrase}"</a><span>({talkAbout[0].mentions} reviews)</span>
         </div>
       </div>
       <div className={styles.keywordRow}>
-        <img src={talkAbout[1].avatar} alt="userPhoto" />
+        <img className={styles.user} src={talkAbout[1].avatar} alt="userPhoto" />
         <div className={styles.text}>
           <a className={styles.phrase}>"{talkAbout[1].phrase}"</a><span>({talkAbout[1].mentions} reviews)</span>
         </div>
       </div>
       <div className={styles.keywordRow}>
-        <img src={talkAbout[2].avatar} alt="userPhoto" />
+        <img className={styles.user} src={talkAbout[2].avatar} alt="userPhoto" />
         <div className={styles.text}>
           <a className={styles.phrase}>"{talkAbout[2].phrase}"</a><span>({talkAbout[2].mentions} reviews)</span>
         </div>
--- client/app/Photos/Photos.jsx
+++ client/app/Photos/Photos.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 import styles from './Photos.css';
 
-const Photos = props => (
-  <div className={styles.photos}>Photos Block</div>
+const Photos = ({ photos }) => (
+  <div className={styles.photos}>
+    <div className={styles.main}>
+      <img src={photos[0].url} alt="main" />
+    </div>
+    <div className={styles.column}>
+      <div className={styles.sidePhoto}>
+        <img src={photos[1].url} alt="main" />
+      </div>
+      <div className={[styles.sidePhoto, styles.mid].join(' ')}>
+        <img src={photos[2].url} alt="main" />
+      </div>
+      <div className={styles.sidePhoto}>
+        <img src={photos[3].url} alt="main" />
+      </div>
+    </div>
+  </div>
 );
 
 export default Photos;
