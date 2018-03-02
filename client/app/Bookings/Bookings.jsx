e807b79fdb67ccb3cd9de448aee8cc641aadad13
Eric M Sandoval <ericmsandoval@gmail.com>
2018-03-02 15:38:09 -0800

    Update layout of bookings and photos components

--- client/app/Bookings/Bookings.jsx
+++ client/app/Bookings/Bookings.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Bookings.css';
 
 const Bookings = props => (
-  <div className={styles.booking}>Booking Block</div>
+  <div className={styles.booking}>Bookings Block</div>
 );
 
 export default Bookings;
