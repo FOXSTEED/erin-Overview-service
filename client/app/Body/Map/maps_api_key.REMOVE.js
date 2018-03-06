ec8305f098d319ea770666f440fa510733a698d9
Eric M Sandoval <ericmsandoval@gmail.com>
2018-03-05 19:43:36 -0800

    Dummy google maps api key file

---
+++ client/app/Body/Map/maps_api_key.REMOVE.js
@@ -0,0 +1,11 @@
+/*
+  Remove '.REMOVE' from this file name, and then replace the key value below to
+  a legitimate Google Maps API key.
+  https://developers.google.com/maps/documentation/javascript/get-api-key
+*/
+
+const container = {
+  key: '123456abcdefg',
+};
+
+export default container;
