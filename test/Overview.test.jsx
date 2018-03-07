a5ff8d8eabccd6dd76b386bebd15b60640c5561f
Eric M Sandoval <ericmsandoval@gmail.com>
2018-03-06 19:05:33 -0800

    Tests if atttraction is open (was previously only testing for closed)

--- test/Overview.test.jsx
+++ test/Overview.test.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import Overview from '../client/app/Overview (Main)/Overview.jsx';
 
 describe('Parent Component', () => {
   test('it should determine if attraction is open or closed', () => {
-    const date = new Date();
-    date.getHours = jest.fn();
-    date.getHours.mockReturnValue = 23;
-
     const wrapper = shallow(<Overview id={0} />);
     const instance = wrapper.instance();
+    const date = new Date();
+
+    date.getHours = jest.fn();
+    date.getHours
+      .mockReturnValueOnce(23)
+      .mockReturnValueOnce(11);
 
     wrapper.instance().determineIfOpen(9, 1, date);
     expect(instance.state.currentlyOpen).toBe(false);
+
+    wrapper.instance().determineIfOpen(9, 1, date);
+    expect(instance.state.currentlyOpen).toBe(true);
   });
 });
