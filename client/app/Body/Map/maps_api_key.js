4b6c7222b2d9293155c5fa45559fa29798c1d876
Eric Sandoval <ericmsandoval@gmail.com>
2018-03-10 10:07:54 -0800

    .env in gitignore

---
+++ client/app/Body/Map/maps_api_key.js
@@ -0,0 +1,11 @@
+/*
+  Remove '.REMOVE' from this file name, and then replace the key value below to
+  a legitimate Google Maps API key.
+  https://developers.google.com/maps/documentation/javascript/get-api-key
+*/
+
+const container = {
+  key: 'AIzaSyChFU1KiqPr2Wdrh-cOOH4rg8rF2JR08Y8',
+};
+
+export default container;
