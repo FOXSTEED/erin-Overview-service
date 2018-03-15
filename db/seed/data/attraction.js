3904b1139a1563c22279401dc8c2c5f911445940
EvaFro <frolli.erin@gmail.com>
2018-03-15 16:37:08 -0700

    impliment 20K in 187 sec using mongoose

--- db/seed/data/attraction.js
+++ db/seed/data/attraction.js
@@ -1,11 +1,11 @@
 const faker = require('faker');
 const random = require('random-ext');
-require('dotenv').config();
 
-const getPhotos = require('./photos');
 const { calculateAverageRating, totalNumberOfReviews } = require('./helpers.js');
 
-const generateSingle = (i, photos) => {
+const attraction = [];
+
+for (let i = 0; i < 10000; i += 1) {
   // ratings array contains numbers of ratings per star amount as follows:
   // [5-stars, 4-stars, 3-stars, 2-stars, 1-star]
   const ratings = [random.integer(200, 0), random.integer(100, 0),
@@ -14,8 +14,7 @@ const generateSingle = (i, photos) => {
   const reviews = totalNumberOfReviews(ratings);
   const randomName = faker.random.words();
   const name = `${randomName} National Park`;
-
-  return {
+  attraction.push({
     id: i,
     name,
     description: faker.lorem.sentences(),
@@ -23,10 +22,10 @@ const generateSingle = (i, photos) => {
     phone: faker.phone.phoneNumberFormat(),
     website: faker.internet.url(),
     email: faker.internet.email(),
-    photos: [{ url: photos[0], comment: faker.lorem.sentence(), user: faker.internet.userName() },
-      { url: photos[1], comment: faker.lorem.sentence(), user: faker.internet.userName() },
-      { url: photos[2], comment: faker.lorem.sentence(), user: faker.internet.userName() },
-      { url: photos[3], comment: faker.lorem.sentence(), user: faker.internet.userName() },
+    photos: [{ url: faker.image.nightlife(), comment: faker.lorem.sentence(), user: faker.internet.userName() },
+      { url: faker.image.nightlife(), comment: faker.lorem.sentence(), user: faker.internet.userName() },
+      { url: faker.image.nightlife(), comment: faker.lorem.sentence(), user: faker.internet.userName() },
+      { url: faker.image.nightlife(), comment: faker.lorem.sentence(), user: faker.internet.userName() },
     ],
     rating,
     ratingBreakdowns: ratings,
@@ -54,26 +53,8 @@ const generateSingle = (i, photos) => {
     opens: random.integer(11, 1),
     closes: random.integer(11, 1),
     duration: `${Math.abs(Math.floor(faker.address.latitude()))} hours`,
-  };
-};
-
-const generateData = async () => {
-  const documentObjects = [];
-  const photos = await getPhotos();
-
-  for (let i = 0; i < 200; i += 1) {
-    const fourPhotos = [];
-    for (let j = 0; j < 4; j += 1) {
-      fourPhotos.push(photos[random.integer(photos.length, 0)]);
-    }
-    const item = generateSingle(i, fourPhotos);
-    documentObjects.push(item);
-  }
-
-  return documentObjects;
-};
-
-const attraction = generateData();
+  });
+}
 
 // Data array contianing seed - documents orginized by Model
 // Format need for mongoose-seed library
