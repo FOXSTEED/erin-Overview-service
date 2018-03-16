dc4b7b4cf46c77c074454d9f6dd492f49eb65c71
EvaFro <frolli.erin@gmail.com>
2018-03-16 12:43:07 -0700

    refactor to use parrel processing 2 seed & mongodb

--- db/seed/seedDB.js
+++ db/seed/seedDB.js
@@ -1,19 +1,62 @@
-const seeder = require('mongoose-seed');
-const data = require('./data/attraction');
-require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const _ = require('ramda');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const dataObj = require('./data/attraction');
 
+const base = parseInt(10000000 / numCPUs);
 
-// Connect to MongoDB via mongoose
-seeder.connect(process.env.OVERVIEW_DATABASE_URL, () => {
-  // load Mongose models
-  seeder.loadModels([
-    'db/seed/data/attractionModel.js',
-  ]);
-  // clear specific collections
-  seeder.clearModels(['Attraction'], () => {
-    // callback to populate DB once collections have cleared
-    seeder.populateModels(data, () => {
-      seeder.disconnect();
-    });
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers and give them starting ID's.
+  for (let i = 0; i < numCPUs; i++) {
+    const baseID = base * i;
+    const worker = cluster.fork();
+    worker.send(baseID);
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} finished`);
   });
-});
+} else {
+
+  process.on('message', (id) => {
+    seedDB(id);
+    console.log(`Worker ${process.pid} started`);
+  });
+}
+
+
+function seedDB(baseID) {
+  MongoClient.connect('mongodb://localhost/').then((client) => {
+    const db = client.db('overview');
+    const collection = db.collection('attractions');
+
+    let count = parseInt(10000000 / numCPUs);
+    const size = 1000;
+    let indexStart = baseID;
+    async function insertBulkArray() {
+      try {
+        const ops = _.range(0, size).map((id) => {
+          return { insertOne: { document: { ...dataObj, id: indexStart + id } } };
+        });
+        await collection.bulkWrite(ops, { ordered: false });
+        count -= size;
+        indexStart += size;
+        if (count > 0) {
+          insertBulkArray();
+        } else {
+          // close mongodb connection
+          client.close();
+          // close worker session
+          process.exit();
+        }
+      } catch (err) {
+        console.log('Seed Error: ', err);
+      }
+    }
+    insertBulkArray();
+  });
+}
+
