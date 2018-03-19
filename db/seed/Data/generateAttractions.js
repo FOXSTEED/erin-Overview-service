f25306da045f729b4dfb1eb0f45e277d22a75278
EvaFro <frolli.erin@gmail.com>
2018-03-19 16:14:14 -0700

    refactor to include ratings as an array

--- db/seed/Data/generateAttractions.js
+++ db/seed/Data/generateAttractions.js
@@ -4,6 +4,8 @@ const random = require('random-ext');
 const { calculateAverageRating, totalNumberOfReviews } = require('./helpers.js');
 
 const generateAttractions = (i) => {
+  const ratings = [random.integer(200, 0), random.integer(100, 0),
+    random.integer(100, 0), random.integer(100, 0), random.integer(100, 0)];
   const rating = calculateAverageRating(ratings);
   const reviews = totalNumberOfReviews(ratings);
   const randomName = faker.random.words();
@@ -18,11 +20,11 @@ const generateAttractions = (i) => {
     website: faker.internet.url(),
     email: faker.internet.email(),
     rating,
-    stars5: random.integer(200, 0),
-    stars4: random.integer(100, 0),
-    stars3: random.integer(100, 0),
-    stars2: random.integer(100, 0),
-    stars1: random.integer(100, 0),
+    stars5: ratings[0],
+    stars4: ratings[1],
+    stars3: ratings[2],
+    stars2: ratings[3],
+    stars1: ratings[4],
     reviews,
     cityrating: random.integer(50, 1),
     cityattractions: random.integer(500, 100),
@@ -38,7 +40,7 @@ function getNextAttractionsData(t, batchIndex) {
   if (batchIndex < 1000) {
     data = new Array(1500);
     for (let i = 0; i < 1500; i++) {
-      data[i] = generateAttractions();
+      data[i] = generateAttractions((i + 1) * batchIndex);
     }
   }
   return Promise.resolve(data);
