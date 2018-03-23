b59e106cf93fc4b4007ba729d668ead085389767
EvaFro <frolli.erin@gmail.com>
2018-03-23 15:35:42 -0700

    edit file path able to run artillery

--- preformance/artillery/artilleryFunctions.js
+++ preformance/artillery/artilleryFunctions.js
@@ -2,7 +2,7 @@ const faker = require('faker');
 
 function randomAttractionID(userContext, events, done) {
   // generate data with faker:
-  const id = faker.random.number({ min: 1, max: 10000000 });
+  const id = faker.random.number({ min: 1, max: 10000 });
   // add variables to virtual user's context:
   userContext.vars.id = id;
   // continue with executing the scenario:
